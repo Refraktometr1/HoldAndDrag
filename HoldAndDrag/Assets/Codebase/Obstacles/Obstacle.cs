446bda46023183b94a48e145aa0d535083412faa
Refraktometr1 <filatovsj@gmail.com>
2023-08-12 19:49:55 +0300

    add sliced obstacle

---
+++ HoldAndDrag/Assets/Codebase/Obstacles/Obstacle.cs
@@ -0,0 +1,48 @@
+﻿using EzySlice;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Codebase.Obstacles
+{
+    public class Obstacle : MonoBehaviour, ISliceable
+    {
+        private void Start()
+        {
+            DoSlice(GetRandomPlane(this.transform.position, this.transform.localScale));
+        }
+
+        public void DoSlice(EzySlice.Plane plane)
+        {
+           var slices = this.gameObject.SliceInstantiate(plane, 
+               new TextureRegion(0.0f, 0.0f, 1.0f, 1.0f),
+               this.GetComponent<Renderer>().material
+               );
+
+           if (slices != null && slices.Length > 0)
+           {
+               foreach (GameObject shatteredObject in slices) 
+               {
+                   shatteredObject.AddComponent<MeshCollider>().convex = true;
+                   shatteredObject.AddComponent<Rigidbody>();
+               }
+           }
+           
+           this.gameObject.SetActive(false);
+        }
+
+        private EzySlice.Plane GetRandomPlane(Vector3 positionOffset, Vector3 scaleOffset) {
+            Vector3 randomPosition = Random.insideUnitSphere;
+
+            //randomPosition += positionOffset;
+
+            Vector3 randomDirection = Random.insideUnitSphere.normalized;
+
+            return new EzySlice.Plane(randomPosition, randomDirection);
+        }
+    }
+
+    public interface ISliceable
+    {
+        public void DoSlice(EzySlice.Plane plane);
+    }
+}
